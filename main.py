e6520a4fdfd903d0466c033e6b23c7ecc0ce81ab
GururajDesai <gururajmdesai@gmail.com>
2023-12-19 19:22:33 +0100

    Integrate the linkedin scrape and use that for llm as input

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import os
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from third_parties.linkedin import scrape_linked_profile
 
 information="""
     Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and investor. He is the wealthiest person in the world, with an estimated net worth of US$222 billion as of December 2023, according to the Bloomberg Billionaires Index, and $244 billion according to Forbes, primarily from his ownership stakes in Tesla and SpaceX.[5][6] He is the founder, chairman, CEO, and chief technology officer of SpaceX; angel investor, CEO, product architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.; founder of the Boring Company and xAI; co-founder of Neuralink and OpenAI; and president of the Musk Foundation.
 """
 
 if __name__ == "__main__":
-    print("Hello Langchain")
-    print(os.environ["OPENAI_API_KEY"])
+    # print("Hello Langchain")
+    # print(os.environ["OPENAI_API_KEY"])
 
     summary_template = """
         given the information {information} about a person from I want you to create:
@@ -22,5 +23,8 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature = 0, model_name ="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    
+    linkedin_data =  scrape_linked_profile("random_url")
 
-    print(chain.run(information=information))
+    print("----- Getting the response ------")
+    print(chain.run(information=linkedin_data))
---
+++ third_parties/linkedin.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+mock_response_url =  ""
+
+def scrape_linked_profile(linkedin_profile_url: str):
+    """
+    scrape information from linkedin profiles.
+    Manually scrape the information from the linkedIn profile
+    """
+    data = None
+    with open('./third_parties/eden_marco_mock.json') as json_data:
+        data = json.load(json_data)
+
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", " ", None)
+            and k not in ["people_also_viewed", "certifications"]
+    }
+
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
