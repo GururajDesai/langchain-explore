2d23f501beb364a7c5db0851dcc455a3cbfcf30e
GururajDesai <gururajmdesai@gmail.com>
2023-12-20 12:09:14 +0100

    Add agents. Linkedin and SerpAPI integration

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from third_parties.linkedin import scrape_linked_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-information="""
+information = """
     Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and investor. He is the wealthiest person in the world, with an estimated net worth of US$222 billion as of December 2023, according to the Bloomberg Billionaires Index, and $244 billion according to Forbes, primarily from his ownership stakes in Tesla and SpaceX.[5][6] He is the founder, chairman, CEO, and chief technology officer of SpaceX; angel investor, CEO, product architect and former chairman of Tesla, Inc.; owner, chairman and CTO of X Corp.; founder of the Boring Company and xAI; co-founder of Neuralink and OpenAI; and president of the Musk Foundation.
 """
 
@@ -12,19 +13,23 @@ if __name__ == "__main__":
     # print("Hello Langchain")
     # print(os.environ["OPENAI_API_KEY"])
 
+    linked_in_profile_url = linkedin_lookup_agent(name="Gururaj Desai")
+
     summary_template = """
         given the information {information} about a person from I want you to create:
         1. a short summary
         2. one interesting fact about them
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
-    
-    llm = ChatOpenAI(temperature = 0, model_name ="gpt-3.5-turbo")
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    
-    linkedin_data =  scrape_linked_profile("random_url")
+
+    linkedin_data = scrape_linked_profile(linked_in_profile_url)
 
     print("----- Getting the response ------")
     print(chain.run(information=linkedin_data))
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-mock_response_url =  ""
+mock_response_url = ""
+
 
 def scrape_linked_profile(linkedin_profile_url: str):
     """
@@ -9,14 +10,14 @@ def scrape_linked_profile(linkedin_profile_url: str):
     Manually scrape the information from the linkedIn profile
     """
     data = None
-    with open('./third_parties/eden_marco_mock.json') as json_data:
+    with open("./third_parties/eden_marco_mock.json") as json_data:
         data = json.load(json_data)
 
     data = {
         k: v
         for k, v in data.items()
         if v not in ([], "", " ", None)
-            and k not in ["people_also_viewed", "certifications"]
+        and k not in ["people_also_viewed", "certifications"]
     }
 
     if data.get("groups"):
